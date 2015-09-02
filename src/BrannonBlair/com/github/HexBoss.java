1c49b50335a40df269b1966a60074b7408b55e48
BrannonBlair <blairb2014@my.fit.edu>
2015-09-02 17:07:51 -0400

    Broken
    Signed-off-by: BrannonBlair <blairb2014@my.fit.edu>

--- src/BrannonBlair/com/github/HexBoss.java
+++ src/BrannonBlair/com/github/HexBoss.java
@@ -9,9 +9,12 @@ import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.PigZombie;
 import org.bukkit.entity.Skeleton;
+import org.bukkit.entity.Zombie;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
@@ -28,26 +31,26 @@ public class HexBoss extends JavaPlugin implements Listener {
 	int taskid2;
 	int taskid3;
 	public boolean dead = true;
-	
+
 	FileConfiguration config = getConfig();
-	
+
 	public int bossInterval = Integer.valueOf(getConfig().getInt("boss_interval"));
-	public int minionInterval =Integer.valueOf(getConfig().getInt("minion_interval"));
+	public int minionInterval = Integer.valueOf(getConfig().getInt("minion_interval"));
 	public String bossSpawn = getConfig().getString("messages.boss_spawn");
 	public String bossDeath = getConfig().getString("messages.boss_death");
-	
+
 	public String bossName = getConfig().getString("boss_settings.boss_name");
 	public int haste = Integer.valueOf(getConfig().getInt("boss_settings.haste_level"));
 	public int speed = Integer.valueOf(getConfig().getInt("boss_settings.speed_level"));
 	public int strength = Integer.valueOf(getConfig().getInt("boss_settings.strength_level"));
 	public int sMaxhealth = Integer.valueOf(getConfig().getInt("boss_settings.max_health"));
 	public int sSethealth = Integer.valueOf(getConfig().getInt("boss_settings.set_health"));
-	
+
 	private String pigName = getConfig().getString("minion_settings.minion_name");
 	private int pigMaxhealth = Integer.valueOf(getConfig().getInt("minion_settings.minion_max_health"));
 	private int pigSethealth = Integer.valueOf(getConfig().getInt("minion_settings.minion_set_health"));
 	private int pigRange = Integer.valueOf(getConfig().getInt("minion_settings.minion_range"));
-	
+
 	String prefix = ChatColor.AQUA + "[" + ChatColor.GREEN + "HexBoss" + ChatColor.AQUA + "] ";
 	int interval = bossInterval;
 	int minutesToCountDown = interval;
@@ -55,8 +58,8 @@ public class HexBoss extends JavaPlugin implements Listener {
 	public void onEnable() {
 		Bukkit.getConsoleSender().sendMessage(prefix + ChatColor.GREEN + "Successfully Started");
 		getServer().getPluginManager().registerEvents(this, this);
-        getConfig().options().copyDefaults(true);  
-        saveConfig();
+		getConfig().options().copyDefaults(true);
+		saveConfig();
 		startMinutesCountdown();
 	}
 
@@ -85,6 +88,7 @@ public class HexBoss extends JavaPlugin implements Listener {
 					HexBoss.this.minutesToCountDown = interval;
 					Bukkit.getServer().getScheduler().cancelTask(taskid2);
 					Bukkit.getServer().getScheduler().cancelTask(taskid3);
+					pigsDie();
 				}
 			}
 		}, 0L, 20L);
@@ -102,6 +106,14 @@ public class HexBoss extends JavaPlugin implements Listener {
 		return;
 	}
 
+	public void pigsDie() {
+		for (Entity en : getServer().getWorld("world").getEntitiesByClasses(PigZombie.class)) {
+			if (((LivingEntity) en).getCustomName().equals(ChatColor.translateAlternateColorCodes('&', pigName)))
+				en.remove();
+		}
+		return;
+	}
+
 	public void onCreatureSpawn(CreatureSpawnEvent event) {
 		if (event.getEntity() instanceof PigZombie) {
 			PigZombie AngryPig = (PigZombie) event.getEntity();
@@ -115,10 +127,10 @@ public class HexBoss extends JavaPlugin implements Listener {
 			public void run() {
 				Random rn = new Random();
 				int range = pigRange;
-				int randomNum =  rn.nextInt(range);
+				int randomNum = rn.nextInt(range);
 				for (int z1 = 1; z1 < randomNum; z1++) {
 					int x = skeleton.getLocation().getBlockX() + rn.nextInt(3 - (-3) + 1) + (-3);
-					int y = skeleton.getLocation().getBlockY()+1;
+					int y = skeleton.getLocation().getBlockY() + 1;
 					int z = skeleton.getLocation().getBlockZ() + rn.nextInt(3 - (-3) + 1) + (-3);
 					World w = getServer().getWorld("world");
 					Location loc = new Location(w, x, y, z);
